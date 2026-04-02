838b244c131ccf26e8b8da413abd2901f6b78c4e
Macbook Pro <macbookpro@MacBook-Air-Macbook.local>
2026-04-02 21:19:27 +0500

    update version

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"78fc3012e45889657f72359b005af7beac47ba
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "34639521"
+    serviceWorkerVersion: "4093112564"
   }
 });
